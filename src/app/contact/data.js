079c892927f04eb6d1a8788d960de8c18ce516ba
Stan Miagchilo <stasss95@mail.ru>
2023-11-03 10:35:52 +0100

    first commit

---
+++ src/app/contact/data.js
@@ -0,0 +1,19 @@
+import floating1 from '../../../public/images/background1.jpg';
+import floating2 from '../../../public/images/float1.png';
+import floating3 from '../../../public/images/float2.jpg';
+import floating4 from '../../../public/images/float3.jpg';
+import floating5 from '../../../public/images/float4.jpg';
+import floating6 from '../../../public/images/background1.jpg';
+import floating7 from '../../../public/images/background1.jpg';
+import floating8 from '../../../public/images/background1.jpg';
+
+export { 
+    floating1, 
+    floating2, 
+    floating3, 
+    floating4, 
+    floating5, 
+    floating6, 
+    floating7, 
+    floating8
+}
